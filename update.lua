1b1fdcd647c96b93c2e37456fe35501d0ffb2fd5
bryangreener <bryancgreener@gmail.com>
2023-08-02 21:48:23 -0400

    test fix

--- update.lua
+++ update.lua
@@ -1,3 +1,4 @@
 local url = "https://bryangreener.github.io/ws_inv/projects/update_files.lua"
 shell.run("wget", url, "update_files.lua")
 shell.run("update_files.lua")
+fs.delete("update_files.lua")
