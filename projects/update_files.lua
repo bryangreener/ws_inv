73fe753f1abd5f4eee6f1a87ef7ed3c49cc17e1c
bryangreener <bryancgreener@gmail.com>
2023-08-02 21:38:55 -0400

    update files

---
+++ projects/update_files.lua
@@ -0,0 +1,20 @@
+print("Pulling changes...")
+
+local url = "https://bryangreener.github.io/ws_inv/"
+
+shell.run("wget", url .. "projects.txt", "projects.txt")
+
+for line in io.lines("projects.txt") do
+    if fs.exists(line) and not fs.isDir(line) then
+        fs.delete(line)
+    end
+    local dir = fs.getDir(line)
+    if not fs.exists(dir) then
+        fs.makeDir(dir)
+    end
+    
+    local new_fp = fs.combine(fs.getDir(line), fs.getName(line))
+    shell.run("wget", url .. new_fp, new_fp)
+end
+
+print("Pulled changes.")
--- update.lua
+++ update.lua
@@ -1,31 +1,3 @@
-print("Pulling changes...")
-
-local url = "https://bryagreener.github.io/ws_inv"
-local p = {}
-
-shell.run(
-    "wget",
-    url .. "projects/projects.txt",
-    "projects.txt"
-)
-
-for line in io.lines("projects.txt") do
-    if fs.exists(line) and not fs.isDir(line) then
-        fs.delete(line)
-    end
-    local dir = fs.getDir(line)
-    if not fs.exists(dir) then
-        fs.makeDir(dir)
-    end
-    
-    shell.run(
-        "wget",
-        url .. fs.combine(
-            fs.getDir(line),
-            fs.getName(line)
-        ),
-        line
-    )
-end
-
-print("Pulled changes.")
+local url = "https://bryangreener.github.io/ws_inv/update_files.lua"
+shell.run("wget", url, "update_files.lua")
+shell.run("update_files.lua")
