198e1832be2db8b0c8278b9f1aa2f39dd51dcf65
bryangreener <bryancgreener@gmail.com>
2023-08-03 13:35:50 -0400

    update update script

--- projects/update_files.lua
+++ projects/update_files.lua
@@ -2,10 +2,49 @@ print("Pulling changes...")
 
 local url = "https://bryangreener.github.io/ws_inv/projects/"
 
-shell.run("wget", url .. "projects.txt", "projects.txt")
-shell.run("wget", url .. "timestamp.txt", "timestamp.txt")
+if not fs.exists("projects") then
+    fs.makeDir("projects")
+end
 
-for line in io.lines("projects.txt") do
+function wget(src, dest)
+    local w = window.create(term.current(), 1, 1, 1, 1, false)
+    term.redirect(w)
+    term.write("Downloading...")
+    shell.run("wget", src, dest)
+    w.clear()
+    term.clear()
+    print("=> " .. dest)
+end
+
+-- First check if we even need to pull any changes.
+wget(url .. "git_hash.txt", "projects/new_git_hash.txt")
+if not fs.exists("projects/git_hash.txt") then
+    fs.move("projects/new_git_hash.txt", "projects/git_hash.txt")
+    fs.delete("projects/new_git_hash.txt")
+else
+    local prev_hash_file = fs.open("projects/git_hash.txt", "r")
+    local prev_hash = prev_hash_file.readAll()
+    prev_hash_file.close()
+
+    local new_hash_file = fs.open("projects/new_git_hash.txt", "r")
+    local new_hash = new_hash_file.readAll()
+    new_hash_file.close()
+
+    fs.delete("projects/git_hash.txt")
+    fs.move("projects/new_git_hash.txt", "projects/git_hash.txt")
+
+    if prev_hash == new_hash then
+        return nil
+    end
+end
+
+-- Now pull the changes.
+local w = window.create(term.current(), 1, 1, 1, 1, false)
+term.redirect(w)
+wget(url .. "projects.txt", "projects/projects.txt")
+wget(url .. "timestamp.txt", "projects/timestamp.txt")
+
+for line in io.lines("projects/projects.txt") do
     if fs.exists(line) and not fs.isDir(line) then
         fs.delete(line)
     end
@@ -15,15 +54,12 @@ for line in io.lines("projects.txt") do
     end
     
     local new_fp = fs.combine(fs.getDir(line), fs.getName(line))
-    shell.run("wget", url .. new_fp, new_fp)
+    wget(url .. new_fp, new_fp)
 end
 
 -- Should only be one line in this file.
-local ts_file = fs.open("timestamp.txt", "r")
+local ts_file = fs.open("projects/timestamp.txt", "r")
 print("TIMESTAMP: " .. ts_file.readAll())
 ts_file.close()
-
-fs.delete("projects.txt")
-fs.delete("timestamp.txt")
 
 print("Pulled changes.")
